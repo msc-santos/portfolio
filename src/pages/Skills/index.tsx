8ba651a03196941738fdced8e81b3a69967e3e16
Marcos1710 <marcossamuel1710@outlook.com>
2023-03-09 21:23:40 -0300

    feat: inserting icons and skill screen

--- src/pages/Skills/index.tsx
+++ src/pages/Skills/index.tsx
@@ -1,5 +1,6 @@
 import { Typography, Container, Grid, useTheme } from "@mui/material";
-import { icons } from "src/constant/iconsSkills";
+import { iconslanguages, iconsFrameworks, iconsTools } from "src/constant/iconsSkills";
+
 import textsPages from 'src/constant/textsPages.json'
 
 export default function Skills() {
@@ -21,10 +22,30 @@ export default function Skills() {
           </Grid>
           <Grid item xs={12} sm={12} md={12} lg={12}>
             <Typography variant="h6" className='text-center' sx={{ color: theme.palette.primary.contrastText }}>
-              {textsPages.skills.technology_frontend}
+              {textsPages.skills.languages}
             </Typography>
-            <Grid container>
-              {icons.map(item => <Grid item xs={4} sm={2} md={1} lg={1}>
+            <Grid container direction="row" justifyContent="center" alignItems="center">
+              {iconslanguages.map(item => <Grid item xs={4} sm={2} md={1} lg={1}>
+                <img src={item.icon} alt={item.alt} className='skill-icons' />
+              </Grid>)}
+            </Grid>
+
+            <br />
+            <Typography variant="h6" className='text-center' sx={{ color: theme.palette.primary.contrastText }}>
+              {textsPages.skills.frameworks_technologies}
+            </Typography>
+            <Grid container direction="row" justifyContent="center" alignItems="center">
+              {iconsFrameworks.map(item => <Grid item xs={4} sm={2} md={1} lg={1}>
+                <img src={item.icon} alt={item.alt} className='skill-icons' />
+              </Grid>)}
+            </Grid>
+
+            <br />
+            <Typography variant="h6" className='text-center' sx={{ color: theme.palette.primary.contrastText }}>
+              {textsPages.skills.tools_latforms}
+            </Typography>
+            <Grid container direction="row" justifyContent="center" alignItems="center">
+              {iconsTools.map(item => <Grid item xs={4} sm={2} md={1} lg={1}>
                 <img src={item.icon} alt={item.alt} className='skill-icons' />
               </Grid>)}
             </Grid>
